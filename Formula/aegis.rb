4790121767599639fa1a53b3057a9982c9671d1e
Ben Ebsworth <benebsworth@MacBook-Pro.modem>
2026-08-25 22:05:11 +1000

    Release Aegis CLI 0.1.1 with Google login

--- Formula/aegis.rb
+++ Formula/aegis.rb
@@ -1,8 +1,8 @@
 class Aegis < Formula
   desc "Securely sync project environment files with your Aegis vault"
   homepage "https://a3gis.me"
-  url "https://github.com/skunkworq/homebrew-tap/releases/download/aegis-v0.1.0/aegis-cli-0.1.0.tar.gz"
-  sha256 "1392bb73846c78376f0c9720c6a13680c60547c42a6abd56bb7c0e87a515a874"
+  url "https://github.com/skunkworq/homebrew-tap/releases/download/aegis-v0.1.1/aegis-cli-0.1.1.tar.gz"
+  sha256 "f93dfba51849010ffa05c4068a08e40fc0e4364d07c3cdbcfd3bee8934b36863"
   depends_on "node@22"
 
   def install
